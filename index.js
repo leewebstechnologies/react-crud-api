bf70773bd2f4cd3385c9289d36a8ceae8176fb21
leewebstechnologies <aliuajibade317@gmail.com>
2024-02-22 23:24:17 +0100

    api completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mysql2 from "mysql2";
 import dotenv from "dotenv";
-
+import cors from "cors";
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ const db = mysql2.createConnection({
 
 // An Express server middleware: It allows us to send any JSON file using a client
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello this is the backend."); //How to make API request using an express server
@@ -30,12 +31,46 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)";
-  const values = [req.body.title, req.body.description, req.body.cover];
+  const q =
+    "INSERT INTO books (`title`, `description`, `price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ];
 
   db.query(q, [values], (error, data) => {
     if (error) return res.json(error);
     return res.json("Book has been created successfully");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (error, data) => {
+    if (error) return res.json(error);
+    return res.json("Book has been deleted successfully");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title` = ?, `description` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (error, data) => {
+    if (error) return res.json(error);
+    return res.json("Book has been updated successfully");
   });
 });
 
